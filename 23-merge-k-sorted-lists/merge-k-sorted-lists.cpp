e2cd937a631859de20882691448a8c9862deded8
Prem Prashant <premprashant120304@gmail.com>
2026-09-21 17:06:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (82.43%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    struct compare {
+    struct cmp {
         bool operator()(ListNode* a, ListNode* b) {
             return a->val > b->val;
         }
     };
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+        for (auto x : lists) if (x) pq.push(x);
 
-        for (ListNode* list : lists) {
-            if (list != nullptr) {
-                pq.push(list);
-            }
-        }
-
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
+        ListNode dummy(0), *cur = &dummy;
 
         while (!pq.empty()) {
-            ListNode* node = pq.top();
-            pq.pop();
-
-            curr->next = node;
-            curr = curr->next;
-
-            if (node->next != nullptr) {
-                pq.push(node->next);
-            }
+            auto x = pq.top(); pq.pop();
+            cur->next = x;
+            cur = x;
+            if (x->next) pq.push(x->next);
         }
 
-        return dummy->next;
+        return dummy.next;
     }
 };
