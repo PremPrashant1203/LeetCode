3bf1b2817f60c994412ec11be8f08a081f322291
Prem Prashant <premprashant120304@gmail.com>
2026-09-23 18:53:11 +0530

    Time: 3 ms (37.02%) | Memory: 14 MB (83.89%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void solve(vector<int>& a, int target, int start, vector<int>& cur, vector<vector<int>>& ans) {
+        if (target == 0) {
+            ans.push_back(cur);
+            return;
+        }
+
+        for (int i = start; i < a.size(); i++) {
+            if (i > start && a[i] == a[i - 1]) continue;
+            if (a[i] > target) break;
+
+            cur.push_back(a[i]);
+            solve(a, target - a[i], i + 1, cur, ans);
+            cur.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+
+        vector<vector<int>> ans;
+        vector<int> cur;
+        solve(candidates, target, 0, cur, ans);
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
